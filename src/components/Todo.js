ba58303d2bd3acd134f5e591305ae39805260213
tingamapuro04 <alponcemobutu100@gmail.com>
2023-01-21 23:34:57 +0300

    add uniqid

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,12 +2,19 @@ import React, { useState } from 'react';
 
 
 
+
 const Todo = () => {
   const [todo, setTodo] = useState("");
+  
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+
   return (
     <div>
       <form action="">
-        <input type="text" name="todo" id="" />
+        <input onChange={handleChange} type="text" name="todo" id="" />
         <input type="submit" value="Add Todo" />
       </form>
     </div>
